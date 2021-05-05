6c2bf121bcd8bfaa5ec28acbcc7579dc566c9fc3
Alex Vakhitov <vakhitovalex@gmail.com>
2021-05-05 13:02:13 -0700

    add userSchema

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const { PORT = 3001 } = process.env;
+
 // connect to the MongoDB server
-mongoose.connect("mongodb://localhost:27017/news", {
+mongoose.connect("mongodb://localhost:27017/news-explorer", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -13,4 +15,6 @@ mongoose.connect("mongodb://localhost:27017/news", {
 
 // connect the middleware, routes, etc...
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`App is working at ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
