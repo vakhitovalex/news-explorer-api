ba0a0ffde89695f95797afa7b7ad6d069ac24979
Alex Vakhitov <vakhitovalex@gmail.com>
2021-05-07 14:50:01 -0700

    add articles schema

---
+++ models/article.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        const linkRegex = /^(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i;
+        return linkRegex.test(link);
+      },
+      message: 'Sorry, your link is incorrect!',
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        const linkRegex = /^(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i;
+        return linkRegex.test(link);
+      },
+      message: 'Sorry, your link is incorrect!',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
